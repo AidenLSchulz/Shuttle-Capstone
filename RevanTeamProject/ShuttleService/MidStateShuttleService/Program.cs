12e2571aaa8ef3020b04ee2049bcec6dc0845eb9
DigitalXcape <146321595+DigitalXcape@users.noreply.github.com>
2026-02-14 12:05:57 -0600

    Implement database migration in Program.cs
    Add database migration on application startup

--- RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
+++ RevanTeamProject/ShuttleService/MidStateShuttleService/Program.cs
@@ -77,6 +77,12 @@ namespace MidStateShuttleService
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
             app.Run();
         }
     }
